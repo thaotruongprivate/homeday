b7f1844159dae297a6709ed979b2f726921dc48e
thaotruongprivate <thao.truong@deliveryhero.com>
2018-04-08 23:15:09 +0200

    display chart

--- public/js/github.js
+++ public/js/github.js
@@ -1,24 +1,96 @@
 $(document).ready(function () {
-    let $repo = $('#repo');
-    $repo.autocomplete({
+    const $repo = $('#repo');
+    const $repoInput = $('#repo-autocomplete');
+    let myChart = null;
+    const $contributors = $('#contributors');
+
+    $repoInput.autocomplete({
         source: function (request, response) {
+            const $repoError = $('.error', $repo);
             $.ajax({
-                url: $repo.attr('data-url').replace('<username>', $repo.val()),
+                method: 'POST',
+                data: {
+                    username: $repoInput.val().split('/')[0],
+                    repo: $repoInput.val().split('/')[1]
+                },
+                beforeSend: function () {
+                    $repoError.html('');
+                },
+                url: $repoInput.attr('data-url'),
                 dataType: "json",
                 success: function (data) {
-                    if (data.message) {
-                        response(data.message);
-                    } else {
-                        response(data.repos);
+                    response(data.repos);
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    const data = JSON.parse(xhr.responseText);
+                    if (data.error) {
+                        $repoError.html(data.error);
                     }
                 }
-            });
+            })
         },
         minLength: 3,
         select: function (event, ui) {
-            // log(ui.item ?
-            //     "Selected: " + ui.item.label :
-            //     "Nothing selected, input was " + this.value);
+            const $contributorError = $('.error', $contributors);
+            const $contributorChart = $('#myChart');
+            const selectedRepo = ui.item.value;
+
+            $.ajax({
+                method: 'POST',
+                data: {
+                    'username': selectedRepo.split('/')[0],
+                    'repo': selectedRepo.split('/')[1]
+                },
+                beforeSend: function () {
+                    $contributors.hide();
+                    $('.error').html('');
+                },
+                url: $repoInput.attr('data-url-contributors'),
+                dataType: "json",
+                success: function (data) {
+                    $contributorChart.show();
+                    drawBarChart(
+                        data.names,
+                        data.contributions,
+                        'Contribution chart of repo ' + selectedRepo
+                    );
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    const data = JSON.parse(xhr.responseText);
+                    $contributorChart.hide();
+                    if (data.error) {
+                        $contributorError.html(data.error);
+                    }
+                }
+            }).complete(function () {
+                $contributors.show();
+            });
         }
     });
+
+    function drawBarChart(xAxis, yAxis, label) {
+        label = label || '';
+        $('#myChart').remove();
+        $contributors.append('<canvas id="myChart"></canvas>');
+        const ctx = document.getElementById('myChart').getContext('2d');
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: xAxis,
+                datasets: [{
+                    label: label,
+                    data: yAxis
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
 });
--- src/Controller/GithubController.php
+++ src/Controller/GithubController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use GuzzleHttp\Client;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -15,10 +19,80 @@ class GithubController extends Controller {
     }
 
     /**
-     * @Route("/github/{username}/repos", name="show_repos", methods={"GET"})
+     * @Route("/github/repos", name="show_repos", methods={"POST"})
      */
-    public function showUserRepos($username) {
-        $url = 'https://api.github.com/users/' . $username . '/repos';
+    public function showUserRepos(Request $request) {
+        $username = $request->get('username');
+        $guzzle = new Client(['http_errors' => false]);
+        $url = 'https://api.github.com/users/' . urlencode($username) . '/repos';
 
+        $response = $guzzle->get($url);
+        $responseBody = json_decode($response->getBody(), true);
+        if ($response->getStatusCode() === Response::HTTP_OK) {
+
+            $repoFilter = strtolower($request->get('repo'));
+            $data = [];
+
+            foreach ($responseBody as $repo) {
+                if (($repoFilter && strpos(strtolower($repo['name']), $repoFilter) !== false) || !$repoFilter) {
+                    $data[] = $repo['full_name'];
+                }
+            }
+
+            if (empty($data)) {
+                return new JsonResponse([
+                    'error' => 'No repos found'
+                ], Response::HTTP_NOT_FOUND);
+            }
+
+            return new JsonResponse([
+                'repos' => $data
+            ]);
+        }
+
+        return new JsonResponse([
+            'error' => $response->getStatusCode() === Response::HTTP_NOT_FOUND ? 'This username is not found' :
+                $responseBody['message']
+        ], $response->getStatusCode());
+    }
+
+    /**
+     * @Route("/github/repos/contributors", methods={"POST"}, name="show_repo_contributors")
+     */
+    public function showRepoContributors(Request $request) {
+        $username = urlencode($request->get('username'));
+        $repoName = urlencode($request->get('repo'));
+        $guzzle = new Client(['http_errors' => false]);
+        $url = "https://api.github.com/repos/{$username}/{$repoName}/contributors";
+
+        $res = $guzzle->get($url);
+        $statusCode = $res->getStatusCode();
+
+        if ($statusCode === Response::HTTP_OK) {
+
+            $body = json_decode($res->getBody(), true);
+            if (empty($body)) {
+                return new JsonResponse([
+                    'error' => 'No contributors found for this repo'
+                ], Response::HTTP_NOT_FOUND);
+            }
+
+            $names = array_map(function($elm) {
+                return $elm['login'];
+            }, $body);
+
+            $contributions = array_map(function($elm) {
+                return $elm['contributions'];
+            }, $body);
+
+            return new JsonResponse([
+                'names' => $names,
+                'contributions' => $contributions
+            ]);
+        }
+
+        return new JsonResponse([
+            'error' => \json_decode($res->getBody(), true)['message']
+        ], $statusCode);
     }
 }
